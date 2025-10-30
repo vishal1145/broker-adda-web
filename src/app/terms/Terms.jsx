cb2613d6f5ad8d4f8c7cb11b2d5247046b2f7eb5
vishal1145 <vishal.kumar1145@gmail.com>
2025-10-30 12:43:19 +0530

    don terms changes-arpita

--- src/app/terms/Terms.jsx
+++ src/app/terms/Terms.jsx
@@ -15,7 +15,7 @@ const Terms = () => {
      <>
       <HeaderFile data={termsData} />
       <div className="py-12">
-    <div className="max-w-7xl mx-auto ">
+    <div className=" mx-auto ">
       {/* <h1 className="text-3xl md:text-4xl font-bold mb-8 text-center text-gray-800">Terms & Conditions</h1> */}
       <div className="space-y-10">
         {terms.map((term, index) => (
